e13b68f55585fcd1ecacec53b6fd6b2361af91c6
Austin <austin.pham.goldenowl@gmail.com>
2021-03-24 17:51:41 +0700

    update testimonials and feedbacks

--- src/utils/feedback.js
+++ src/utils/feedback.js
@@ -2,8 +2,10 @@
 
 import JonatanLindstrom from '../assets/images/feedback/JonatanLindstrom.png';
 import ericFitzgerald from '../assets/images/feedback/EricFitzgerald.png';
-import antony from '../assets/images/feedback/Antony.png';
 import sky from '../assets/images/feedback/Sky.png';
+import auston from '../assets/images/feedback/auston.png';
+import nigel from '../assets/images/feedback/nigel.png';
+import kristian from '../assets/images/feedback/kristian.png';
 
 const feedback = [
   {
@@ -23,6 +25,24 @@ const feedback = [
     project: 'Founder & CEO at Cheer Me',
     url: ericFitzgerald,
     content: 'I would recommend Golden Owl 100%. They surprised us with their speed and brought really talented people to help us out. We appreciate their knowledge, engagement, and dedication.',
+  },
+  {
+    name: 'Auston Lende',
+    project: 'Senior Market Planning Manager at Starbucks',
+    url: auston,
+    content: 'I have been working with GoldenOwl, for a year now and I am very impressed with the capability and professionalism of each person I’ve worked with',
+  },
+  {
+    name: 'Nigel Pua',
+    project: 'Co-founder at TheNightMrkt',
+    url: nigel,
+    content: 'They were receptive to minor changes that we requested on the fly! Bugs were fixed with no further issues, new features were developed according to specifications, work was done and delivered promptly',
+  },
+  {
+    name: 'Kristian Gerardsson',
+    project: 'Frontend Team Lead at Currenxie',
+    url: kristian,
+    content: "We've been working with Golden Owl for 9 months straight, and we'll continue to do so. We are having a great experience with their (react/react-native) developer, working with us remotely. We can definitely recommend anyone looking for remote developers to try Justin's software development team.",
   },
 ];
 
--- src/utils/testimonials.js
+++ src/utils/testimonials.js
@@ -1,45 +1,32 @@
 /* eslint-disable  max-len */
 
-import antony from '../assets/images/testimonials/testimonialItems/1.png';
-import mark from '../assets/images/testimonials/testimonialItems/2.png';
-import jane from '../assets/images/testimonials/testimonialItems/3.png';
-import riaz from '../assets/images/testimonials/testimonialItems/4.png';
-import myAgency from '../assets/images/testimonials/testimonialItems/5.png';
-import refferalCandy from '../assets/images/testimonials/testimonialItems/6.png';
-import bid4Skill from '../assets/images/testimonials/testimonialItems/7.png';
-import gig from '../assets/images/testimonials/testimonialItems/8.png';
+import jonatan from '../assets/images/feedback/JonatanLindstrom.png';
+import auston from '../assets/images/feedback/auston.png';
+import kristian from '../assets/images/feedback/kristian.png';
 
 export default {
-  'Antony Woodcock': {
-    avatar: antony,
-    main: 'They’re excellent went beyond expectations.',
-    details: '"The end product was excellent, beyond my expectations. Golden Owl is the first company I would recommend to other people. I’m definitely confident for future partnership."',
-    companyLogo: myAgency,
-    title: 'CO - FOUNDER & MANAGER DIRECTOR OF MY AGENCY',
-    url: 'my-work',
+  'Jonatan Lindström': {
+    avatar: jonatan,
+    main: 'They work hard and exhibit skills that ease up the overall process for both parties.',
+    details: '"Golden Own Consulting Ltd is keen on submitting deliverables on time in order to help the client meet their project goals within the timeframe. They work hard and exhibit skills that ease up the overall process for both parties. The client commends them for their promptness and coding skills."',
+    // companyLogo: collabs,
+    title: 'FULL-STACK ENGINEER AT COLLABS',
+    url: 'collabs',
   },
-  Mark: {
-    avatar: mark,
-    main: 'High quality engineers and professional working style.',
-    details: '"They demonstrated a greater understanding of our business goals than other companies had shown in the past. The developers from Golden Owl Consulting impressed me the most with their dedication and expertise. They provide a tremendous level of service which is worth every penny.”',
-    companyLogo: refferalCandy,
-    title: 'CO - FOUNDER & ADVISORY OF REFERRAL CANDY',
-    url: 'referral-candy',
+  'Auston Lende': {
+    avatar: auston,
+    main: 'Impressed with the capability and professionalism of each person',
+    details: '"I have been working with GoldenOwl, for a year now and I am very impressed with the capability and professionalism of each person I’ve worked with.”',
+    // companyLogo: refferalCandy,
+    title: 'SENIOR MARKET PLANNING MANAGER AT STARBUCKS',
+    url: 'open-the-app',
   },
-  Jane: {
-    avatar: jane,
-    main: 'They showed a great passion and can-do attitude during the project.',
-    details: '"The team they sent out was amazing. They took everything personal, showed a great passion for our project like this was their own. I told them what I needed, and they made it happen, never to shoot me down by telling me that it won’t work.”',
-    companyLogo: bid4Skill,
-    title: 'CO - FOUNDER & MANAGER DIRECTOR OF BID4SKILL',
-    url: 'bid-4-skill',
-  },
-  'Riaz Ladha': {
-    avatar: riaz,
-    main: 'They have great communication and problem-solving skills',
-    details: '"Partnering with the Golden Owl Consulting team is definitely a good choice. I just came to them with an idea and they provide me with a detailed roadmap to achieve my vision. There isn\'t a language barrier to overcome, and everyone is proactive, helpful, and ethical. No need to mention their attention to detail, productivity and creativity.”',
-    companyLogo: gig,
-    title: 'CO - FOUNDER & ADVISORY OF GIG TO GIG',
-    url: 'gig',
+  "Kristian Gerardsson": {
+    avatar: kristian,
+    main: "We've been working with Golden Owl for 9 months straight, and we'll continue to do so.",
+    details: `"We've been working with Golden Owl for 9 months straight, and we'll continue to do so. We are having a great experience with their (react/react-native) developer, working with us remotely. We can definitely recommend anyone looking for remote developers to try Justin's software development team.”`,
+    // companyLogo: currenxie,
+    title: 'FRONTEND TEAM LEAD AT CURRENXIE',
+    // url: 'bid-4-skill',
   },
 };
