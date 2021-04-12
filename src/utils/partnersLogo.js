5996ffbb8273c2370e2cd43e55e58e1aa597e091
franklin-le-goldenowl <franklin.le.goldenowl@gmail.com>
2021-04-12 09:21:26 +0700

    Change UI add project logo

--- src/utils/partnersLogo.js
+++ src/utils/partnersLogo.js
@@ -1,94 +1,64 @@
-import assured from '../assets/images/partner/1.png';
-import finda from '../assets/images/partner/2.png';
-import luxe from '../assets/images/partner/3.png';
 import candy from '../assets/images/partner/4.png';
 import gig from '../assets/images/partner/5.png';
-import myWork from '../assets/images/partner/6.png';
 import alexWorld from '../assets/images/partner/7.png';
-import bid4Skill from '../assets/images/partner/8.png';
-import briidge from '../assets/images/partner/9.png';
-import connectAJob from '../assets/images/partner/10.png';
-import homeHub from '../assets/images/partner/11.png';
-import mosendo from '../assets/images/partner/12.png';
-import nexlec from '../assets/images/partner/13.png';
-import omniClean from '../assets/images/partner/14.png';
-import personifi from '../assets/images/partner/15.png';
-import publicInsight from '../assets/images/partner/16.png';
 import theWorkProject from '../assets/images/partner/17.png';
-import zetya from '../assets/images/partner/18.png';
+import prudential from '../assets/images/partner/19.png';
+import narratrs from '../assets/images/partner/20.png';
+import omniFM from '../assets/images/partner/21.png';
+import laku6 from '../assets/images/partner/22.png';
+import biib from '../assets/images/partner/23.png';
+import vidy from '../assets/images/partner/24.png';
+import glife from '../assets/images/partner/25.png';
+import expiwell from '../assets/images/partner/26.png';
 
 const partnersLogo = [
   {
-    name: 'assured',
-    url: assured,
-  },
-  {
-    name: 'find a wealth',
-    url: finda,
-  },
-  {
-    name: 'luxe',
-    url: luxe,
-  },
-  {
-    name: 'referral candy',
-    url: candy,
+    name: 'prudential',
+    url: prudential,
   },
   {
     name: 'gig',
     url: gig,
   },
   {
-    name: 'mywork',
-    url: myWork,
-  },
-  {
-    name: 'alexWorld',
-    url: alexWorld,
-  },
-  {
-    name: 'bid4skill',
-    url: bid4Skill,
-  },
-  {
-    name: 'briidge',
-    url: briidge,
-  },
-  {
-    name: 'connectAJob',
-    url: connectAJob,
-  },
-  {
-    name: 'homeHub',
-    url: homeHub,
-  },
-  {
-    name: 'mosendo',
-    url: mosendo,
-  },
-  {
-    name: 'nexlec',
-    url: nexlec,
-  },
-  {
-    name: 'omniClean',
-    url: omniClean,
-  },
-  {
-    name: 'personifi',
-    url: personifi,
-  },
-  {
-    name: 'publicInsight',
-    url: publicInsight,
+    name: 'referral candy',
+    url: candy,
   },
   {
     name: 'theWorkProject',
     url: theWorkProject,
   },
   {
-    name: 'zetya',
-    url: zetya,
+    name: 'narratrs',
+    url: narratrs,
+  },
+  {
+    name: 'omniFM',
+    url: omniFM,
+  },
+  {
+    name: 'laku6',
+    url: laku6,
+  },
+  {
+    name: 'biib',
+    url: biib,
+  },
+  {
+    name: 'vidy',
+    url: vidy,
+  },
+  {
+    name: 'alexWorld',
+    url: alexWorld,
+  },
+  {
+    name: 'glife',
+    url: glife,
+  },
+  {
+    name: 'expiwell',
+    url: expiwell,
   },
 ];
 
